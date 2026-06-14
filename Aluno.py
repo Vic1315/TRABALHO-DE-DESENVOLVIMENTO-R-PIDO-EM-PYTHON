990acadb00a2f6245be9a3daa7d2355a3264397b
Vic1315 <victorvincent0897@gmail.com>
2026-06-13 22:10:17 -0300

    Fix missing grades for aluno2 and add print loop

---
+++ Aluno.py
@@ -0,0 +1,14 @@
+class Aluno:
+   def__init__(self: object, nome: str, idade: int, notas: list) -> None:
+      self.nome = nome
+      self.idade = idade
+      self.notas = notas
+def calcular_media(self.notas) / len(self.notas)
+    return sum(self.notas) / len(self.notas)
+
+if__name__== "__main__":
+   aluno1 = Aluno("Raphael", 41, [9.5, 8.7, 7.2])
+   aluno2 = Aluno("Caroline", 32,[6.8, 7.5, 8.9])
+   lista_alunos = [aluno1, aluno2]
+   for aluno in lista_alunos:
+       print(f"Nome: {aluno.nome}, Média {aluno.calcular_media():.2f}")
